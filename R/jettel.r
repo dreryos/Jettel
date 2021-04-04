17fc8e51008667b7968af6907a434de61a5538f1
dreryos <marek.foldy@gmail.com>
2021-04-04 17:50:16 +0200

    added search selector

--- R/jettel.r
+++ R/jettel.r
@@ -120,8 +120,12 @@ retrieve.plant <- function(token = 0, plant = 0){
     if(!(user_sel %in% poss)){
         print("Invalid option selected")
     }else{
-        return(possibilities$data.id)
+        return(possibilities[user_sel, "data.id"])
     }
+}
+
+get.plant.by.id <- function(token = 0, plant = 0){
+
 }
 
 #' Frontend GUI for this package
@@ -135,31 +139,78 @@ retrieve.plant <- function(token = 0, plant = 0){
 jettel.gui <- function(){
     search.query <- tcltk::tclVar("")
     key <- tcltk::tclVar("")
+    key.status <- tcltk::tclVar("")
 
     key.gui <- function(){
        key.root <- tcltk::tktoplevel(gui)
-       key.eneter.frame <- 
-       key.enter <- tcltk::tkentry(key.root, textvariable = key)
-       key.but <- tcltk::tkbutton(key.root, text="Enter")
+       key.lab <- tcltk::tklabel(key.root, text = "Enter your API key")
+       key.obtain.frame <- tcltk::tkframe(key.root)
+       key.obtain.lab <- tcltk::tklabel(key.obtain.frame, text = "Key can be obtained:")
+       key.obtain.but <- tcltk::tkbutton(key.obtain.frame, text = "here", relief = "flat", fg="#0900ff" , command = function(){
+           utils::browseURL("https://trefle.io/users/sign_up")
+       })
+       key.enter.frame <- tcltk::tkframe(key.root)
+       key.enter <- tcltk::tkentry(key.enter.frame, textvariable = key)
+       key.but <- tcltk::tkbutton(key.enter.frame, text="Enter", command = function(){
+           if(tcltk::tclvalue(key) != ""){
+            #unblocks Search button and hide Enter key button
+           if(tcltk::tclvalue(key.check(tcltk::tclvalue(key))) == "Key is OK"){
+                key.status <- key.check(tcltk::tclvalue(key))
+                tcltk::tkpack.forget(key.check.lab, key.check.but, search.button)
+                key.check.lab <- tcltk::tkmessage(key.check.frame, text = tcltk::tclvalue(key.status))
+                search.button <- tcltk::tkbutton(search.frame, text = "Push", command = function(){
+                        response <- search.plant(tcltk::tclvalue(key), tcltk::tclvalue(search.query))
+                        resp.win <- tcltk::tktoplevel(gui)
+                        resp.lab <- tcltk::tklabel(resp.win, text = "Select entry by double-click")
+                        list <- tcltk::tklistbox(resp.win, listvariable = tcltk::tclVar(response$data.scientific_name), selectmode = "single")
+                        tcltk::tkbind(list, '<Double-Button>', function(){
+                            entry.win <- tcltk::tktoplevel(resp.win)
+                            entry.label <- tcltk::tklabel(entry.win, text = paste("Scientific name: ", tcltk::tclvalue(tcltk::tkXselection.get())))
+                            entry.id <- tcltk::tklabel(entry.win, text = paste("ID: ", response$data.id[response$data.scientific_name == tcltk::tclvalue(tcltk::tkXselection.get())]))
+                            tcltk::tkpack(entry.label, entry.id)
+                        })
+                        tcltk::tkpack(resp.lab,list)
+                    }, state = "active")
+                tcltk::tkpack(key.check.lab, search.button, side = "left")
+                tcltk::tkdestroy(key.root)
+            }           
+           }else{
+                tcltk::tkmessageBox(icon = "error", message = "Key field can't be empty!", type = "ok")
+           }})
+       tcltk::tkpack(key.lab, key.obtain.frame, key.enter.frame)
+       tcltk::tkpack(key.obtain.lab, key.obtain.but, side = "left")
        tcltk::tkpack(key.enter, key.but, side = "left")
     }
 
+    #checks the API key
     key.check <- function(token){
         if(token == ""){
-            return("Enter API key!")
-        }else{
-            return("Key is OK")
+            return(tcltk::tclVar("Enter API key!"))
+        }
+        if(token != ""){
+            res <- httr::GET(paste(base_url, "kingdoms", '?', 'token', '=', token, sep=''))
+            if(httr::status_code(res) != 200){
+                if(httr::status_code(res) == 401){
+                    tcltk::tkmessageBox(icon = "error", message = "You've entered invalid API key")
+                }else{
+                    tcltk::tkmessageBox(icon = "error", message = paste("There\'s an error:", httr::http_status(httr::status_code(res))$message))
+                }
+            }else{        
+                return(tcltk::tclVar("Key is OK"))
+            }            
         }
     }
+
+    #Start gui
     gui <- tcltk::tktoplevel()
     gui.wel <- tcltk::tklabel(gui, text = "Welcome to Trefle search")    
     key.check.frame <- tcltk::tkframe(gui)    
-    key.check.lab <- tcltk::tkmessage(key.check.frame, text = key.check(tcltk::tclvalue(key)))
+    key.check.lab <- tcltk::tkmessage(key.check.frame, text = tcltk::tclvalue(key.check(tcltk::tclvalue(key))))
     key.check.but <- tcltk::tkbutton(key.check.frame, text="Enter key", command = key.gui)
     search.frame <- tcltk::tkframe(gui)
     search.lab <- tcltk::tklabel(search.frame, text = "Enter search query:")
     search.entry <- tcltk::tkentry(search.frame, textvariable = search.query)
-    search.button <- tcltk::tkbutton(search.frame, text = "Push", command = function(){print(as.character(tcltk::tclvalue(search.query)))})
+    search.button <- tcltk::tkbutton(search.frame, text = "Push", state = "disabled")
     tcltk::tkpack(gui.wel, key.check.frame, search.frame, side = "top")
     tcltk::tkpack(key.check.lab, key.check.but, side = "left")
     tcltk::tkpack(search.lab, search.entry, search.button, side = "left")
